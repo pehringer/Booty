5133e10e7cc1fef52e503cf51d3ba04f600e1b47
pehringer <pehringer@outlook.com>
2023-08-27 17:08:58 -0600

    Better docs and assembly comments.

--- 16-bit/hello16.asm
+++ 16-bit/hello16.asm
@@ -1,39 +1,62 @@
-bits 16    ; On startup the cpu is configured like a 16-bit 8086 chip from 1978.
-           ; Tell NASM the following lines are 16-bit instructions.
+; Tell NASM to we are now compiling 16-bit assembly.
+bits 16
+
+; The BIOS will load the drives first sector into memory at address 0x7C00.
+; Tell NASM to generate machine code starting at address 0x7c00.
+org 0x7c00
+
+; Some BIOS's will load the first sector into memory at address 0x0000:0x7C00.
+; Other will load the first sector into memory at address 0x07C0:0x0000.
+; Far jump to fix this issue (reload CS to 0x0000).
+jmp 0x0000:boot16
+
+; Use MASM to write hello string in memory.
+hello_string:
+    db "hello",0
 
 
-org 0x7c00    ; The BIOS will load the drives first sector (512 bytes) into RAM
-              ; at address 0x7C00, The BIOS will then jumping execution to address
-              ; 0x7C00. Tell NASM to generate machine code starting at address 0x7c00.
+
+; We are going to use the BIOS interrupt 0x10 (video services) to
+; print chars to the screen. Like any interrupt we need to first load
+; its arguments into a few predetermined locations:
+;   - Register ah = Operation.
+;   - Register al = ASCII character to print.
 
 
-jmp 0x0000:boot16    ; Some BIOS's will load to 0x0000:0x7C00, Other will load load to
-                      ; 0x07C0:0x0000. Far jump to fix this issue (reload CS to 0x0000).
 
+boot16:
+    ; Use for BIOS interrupt (print ASCII character, second byte of AX).
+    mov ah,0x0E
 
-boot16:            ; We are going to use the BIOS interrupt 0x10 (video services) to
-    mov ah,0x0e     ; print chars to the screen. Like any interrupt we need to first load
-    mov si,hello    ; its arguments into a few predetermined locations:
-    xor al,al       ;   - Register ah = Operation.
-loop:              ;   - Register al = ASCII character to print.
+    ; Init pointer to hello string.
+    mov si,hello_string
+
+print_character_loop:
+    ; load in character (first byte of AX)
     mov al,[si]
-    inc si
+
+    ; If character if NULL then exit loop.
     or al,al
     jz halt
+
+    ; Call BIOS interrupt to wrtie character to screen.
     int 0x10
-    jmp loop
+
+    ; Increment pointer to next character.
+    inc si
+
+    ; Loop again.
+    jmp print_character_loop
+
 halt:
+    ; Disable interrupts and halt.
     cli
     hlt
 
+; Use NASM to fill in the rest of this 512 byte sector (except for the last 2
+; bytes) with zeros.
+times 510 - ($-$$) db 0
 
-hello: db "hello",0    ; Tell NASM to write a string literal and null terminator at this
-                       ; location in memory (db = define byte).
-
-
-times 510 - ($-$$) db 0    ; Tell NASM to fill in the rest of this 512 byte sector (except for
-                           ; the last 2 bytes) with zeros (db - define byte).
-
-
-dw 0xaa55    ; Tell NASM to write two bytes, this word at the end of the sector
-             ; marks the sector as executable (dw = define word).
+; Use NASM to write two bytes, this word at the end of the sector marks the
+; sector as executable.
+dw 0xaa55
--- 32-bit/hello32.asm
+++ 32-bit/hello32.asm
@@ -1,10 +1,16 @@
+; Tell NASM to we are now compiling 16-bit assembly.
 bits 16
+
+; The BIOS will load the drives first sector into memory at address 0x7C00.
+; Tell NASM to generate machine code starting at address 0x7c00.
 org 0x7c00
 
-
+; Some BIOS's will load the first sector into memory at address 0x0000:0x7C00.
+; Other will load the first sector into memory at address 0x07C0:0x0000.
+; Far jump to fix this issue (reload CS to 0x0000).
 jmp 0x0000:boot16
 
-
+; Use NASM to write global descriptor table in memory.
 global_descriptor_table_start:
 null_segment_descriptor_entry:
     dw 0000000000000000b
@@ -21,67 +27,100 @@ code_segment_descriptor_entry:
     db 11001111b
     db 00000000b
 data_segment_descriptor_entry:
-    dw 0xFFFF
-    dw 0x0
-    db 0x0
+    dw 1111111111111111b
+    dw 0000000000000000b
+    db 00000000b
     db 10010010b
     db 11001111b
-    db 0x0
+    db 00000000b
 global_descriptor_table_end:
 
-
+; Have NASM define a few useful values.
 TABLE_SIZE equ global_descriptor_table_end - global_descriptor_table_start
 TABLE_OFFSET equ global_descriptor_table_start
 CODE_ENTRY_OFFSET equ code_segment_descriptor_entry - global_descriptor_table_start
 DATA_ENTRY_OFFSET equ data_segment_descriptor_entry - global_descriptor_table_start
 
-
+; Use NASM to write global descriptor table pointer in memory.
 global_descriptor_table_pointer:
     dw TABLE_SIZE
     dd TABLE_OFFSET
 
+; Use MASM to write hello string in memory.
+hello_string:
+    db "hello",0
 
 boot16:
+    ; Use BIOS interrupt to set VGA Test mode 3.
     mov ax, 0x3
     int 0x10
 
-    cli                                       ; 1) disable interrupts
+    ; (Config 32-bit Step 1) Disable interrupts.
+    cli
 
-    mov ax, 0x2401                            ; 2) enable A20 line
-    int 0x15                                  ;
+    ; (Config 32-bit Step 2) Enable A20 line.
+    mov ax, 0x2401
+    int 0x15
 
-    lgdt [global_descriptor_table_pointer]    ; 3) load the gdt table
+    ; (Config 32-bit Step 3) load the gdt table.
+    lgdt [global_descriptor_table_pointer]
 
-    mov eax, cr0                              ; 4) set the protected mode bit on special CPU reg cr0
-    or eax,0x1                                ;
-    mov cr0, eax                              ;
+    ; (Config 32-bit Step 4) Set the protected mode bit in cr0 register.
+    mov eax, cr0
+    or eax,0x1
+    mov cr0, eax
 
-    jmp CODE_ENTRY_OFFSET:boot32              ; 5) long jump to the code segment
+    ; (Config 32-bit Step 5) long jump to the code segment.
+    jmp CODE_ENTRY_OFFSET:boot32
 
-
+; Tell NASM to we are now compiling 32-bit assembly.
 bits 32
-boot32:
-    mov ax, DATA_ENTRY_OFFSET                 ; 6) load data segment registers with valid selector
-    mov ds, ax                                ;
-    mov es, ax                                ;
-    mov fs, ax                                ;
-    mov gs, ax                                ;
-    mov ss, ax                                ;
 
+boot32:
+    ; (Config 32-bit Step 6) load data segment registers with valid selector
+    mov ax, DATA_ENTRY_OFFSET
+    mov ds, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+    mov ss, ax
+
+    ; Init pointer to hello string.
     mov esi,hello_string
-    mov ebx,0xb8000
+
+    ; Init pointer to VGA text buffer.
+    mov edi,0xb8000
+
+    ; Set in character color to white (second byte of EAX).
+    mov ah,0xF
+
 print_character_loop:
-    lodsb
+    ; load in character (first byte of EAX).
+    mov al,[esi]
+
+    ; If character if NULL then exit loop.
     or al,al
     jz halt
-    or eax,0x0100
-    mov word [ebx], ax
-    add ebx,2
+
+    ; Write to character data to VGA text buffer (first two bytes of EAX)
+    mov [edi], ax
+
+    ; Increment pointers to next character.
+    add edi,0x2
+    inc esi
+
+    ; Loop again.
     jmp print_character_loop
+
 halt:
+    ; Disable interrupts and halt.
+    cli
     hlt
-hello_string: db "Hello world!",0
 
-
+; Use NASM to fill in the rest of this 512 byte sector (except for the last 2
+; bytes) with zeros.
 times 510 - ($-$$) db 0
+
+; Use NASM to write two bytes, this word at the end of the sector marks the
+; sector as executable.
 dw 0xaa55
